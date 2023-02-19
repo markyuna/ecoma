1f9a14ece637d07fe787e371c7c2b27bee15d828
Emmanuel Dedryver <marcossuarezr88@gmail.com>
2023-02-19 02:34:07 +0100

    translate devise to french done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :set_locale
+
+
+  private
+
+  def set_locale
+    I18n.locale = :fr # Or whatever logic you use to choose.
+  end
 end
