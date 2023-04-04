4d8333b77f8d42fd10b58d8e86cb0c2ec4ae54ce
Emmanuel Dedryver <marcossuarezr88@gmail.com>
2023-04-04 16:06:39 +0200

    home page updated

--- app/javascript/controllers/loading_controller.js
+++ app/javascript/controllers/loading_controller.js
@@ -8,6 +8,6 @@ export default class extends Controller {
   loading(event) {
     console.log('loading')
     console.log(event.currentTarget)
-    event.currentTarget.parentElement.innerHTML = "<div class='lds-ring'><div></div><div></div><div></div><div></div></div><h4 class='souffle'>Reprenez Votre Souffle ! Medi est la pour vous ...</h4></div>";
+    event.currentTarget.parentElement.innerHTML = "<div class='lds-ring'><div></div><div></div><div></div><div></div></div><h4 class='souffle'>L O A D I N G ...</h4></div>";
   }
 }
